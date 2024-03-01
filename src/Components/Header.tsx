83fe1c8b2e595da5754a98953cde23257c84641b
mateus leonardo <mateus_leonardo1997@hotmail.com>
2024-03-01 11:19:04 -0300

    adicionando mesBtn

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,12 +1,16 @@
-import React from "react"
-import DateRange from "./DateRange"
+import React from "react";
+import DateRange from "./DateRange";
+import Meses from "./Meses";
 
 const Header = () => {
   return (
-    <div>
-      <DateRange/>
+    <div className="mb">
+      <div className="mb">
+        <DateRange />
+      </div>
+      <Meses />
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
