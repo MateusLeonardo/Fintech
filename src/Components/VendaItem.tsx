ff58215c21a3b01df9e640c4adc7c3ae9425f20b
mateus leonardo <mateus_leonardo1997@hotmail.com>
2024-03-03 00:00:41 -0300

    adicionando loading

--- src/Components/VendaItem.tsx
+++ src/Components/VendaItem.tsx
@@ -1,13 +1,21 @@
-import { IVenda } from '../Context/DataContext'
+import { NavLink } from "react-router-dom";
+import { IVenda } from "../Context/DataContext";
 
-const VendaItem = ({venda}: {venda: IVenda}) => {
+const VendaItem = ({ venda }: { venda: IVenda }) => {
   return (
-    <div className='venda box'>
-      <a href="" style={{fontFamily: 'monospace'}}>{venda.id}</a>
+    <div className="venda box">
+      <NavLink to={`/vendas/${venda.id}`} style={{ fontFamily: "monospace" }}>
+        {venda.id}
+      </NavLink>
       <div>{venda.nome}</div>
-      <div>{venda.preco.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}</div>
+      <div>
+        {venda.preco.toLocaleString("pt-br", {
+          style: "currency",
+          currency: "BRL",
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default VendaItem
+export default VendaItem;
